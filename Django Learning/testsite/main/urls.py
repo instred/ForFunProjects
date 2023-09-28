8447e03b2e4679861fe0b139f5f57e27d823e652
instred <jakub.bok23@gmail.com>
2023-09-28 14:17:19 +0200

    added login + footer

--- Django Learning/testsite/main/urls.py
+++ Django Learning/testsite/main/urls.py
@@ -5,5 +5,6 @@ urlpatterns = [
     path("<int:id>", views.index, name='index'),
     path("", views.home, name="home"),
     path("create/", views.create, name='create'),
-    path("papa/", views.papa, name='papa')
+    path("papa/", views.papa, name='papa'),
+    path("view/", views.view, name='view'),
 ]
--- Django Learning/testsite/main/views.py
+++ Django Learning/testsite/main/views.py
@@ -27,27 +27,30 @@ def papa(response):
 def index(response, id):
     ls = MyList.objects.get(id=id)
 
-    if response.method == "POST":
-        print(response.POST)
-        
-        if response.POST.get("save"):
-            for item in ls.item_set.all():
-                if response.POST.get("c" + str(item.id)) == "clicked":
-                    item.complete = True
+    if ls in response.user.mylist.all():
+
+        if response.method == "POST":
+            print(response.POST)
+            
+            if response.POST.get("save"):
+                for item in ls.item_set.all():
+                    if response.POST.get("c" + str(item.id)) == "clicked":
+                        item.complete = True
+                    else:
+                        item.complete = False
+                    
+                    item.save()
+
+            elif response.POST.get("newItem"):
+                txt = response.POST.get("new")
+
+                if len(txt) > 2:
+                    ls.item_set.create(text=txt, complete=False)
                 else:
-                    item.complete = False
-                
-                item.save()
+                    print("invalid")
 
-        elif response.POST.get("newItem"):
-            txt = response.POST.get("new")
-
-            if len(txt) > 2:
-                ls.item_set.create(text=txt, complete=False)
-            else:
-                print("invalid")
-
-    return render(response, "main/list.html", {"ls": ls})
+        return render(response, "main/list.html", {"ls": ls})
+    return  render(response, "main/view.html", {})
 
 def home(response):
     return render(response, "main/home.html", {})
@@ -58,13 +61,17 @@ def create(response):
 
         if form.is_valid():
             n = form.cleaned_data["name"]
-            l = MyList(name=n)
-            l.save()
-        return HttpResponseRedirect("/%i" %l.id)
+            t = MyList(name=n)
+            t.save()
+            response.user.mylist.add(t)
+        return HttpResponseRedirect("/%i" %t.id)
 
 
     else:
         form = CreateList()
     return render(response, "main/create.html", {"form":form})
+
+def view(response):
+    return render(response, "main/view.html", {})
 
 
